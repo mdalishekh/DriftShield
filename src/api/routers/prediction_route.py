46b9b32c9f90fdae1498864e3e994a037824c3ea
MD ALI SHEKH <mdalishekh2003@gmail.com>
2026-06-05 15:38:33 +0530

    Risk calculation and smart loan suggestions to be implemented

--- src/api/routers/prediction_route.py
+++ src/api/routers/prediction_route.py
@@ -11,8 +11,11 @@ router = APIRouter(prefix="/prediction", tags=["Predictions"])
 @router.post("/", response_model=PredictionResponse)
 def predict(data: PredictionRequest):
     logger.info("Prediction request received")
+    
     result = predict_default(data.model_dump())
+    
     logger.info(f"Prediction result: {result}")
+    
     with db_connect() as db:
         logger.info("Inserting prediction into database")
         insert_prediction(
@@ -21,7 +24,10 @@ def predict(data: PredictionRequest):
             predicted_default=result["default"],
             probability= float(result["probability"])
         )
+        
+        
         logger.info("Prediction inserted into database successfully")
+        
     return PredictionResponse(
         status="success",
         prediction=result # type: ignore
