dfad499ca670186bdb3e0fe29cb31a3e743d36cb
MD ALI SHEKH <mdalishekh2003@gmail.com>
2026-06-15 14:31:07 +0530

    # V1 Phase 2 Drift detection API completed along with LLM insights

--- src/api/routers/drift_route.py
+++ src/api/routers/drift_route.py
@@ -1,15 +1,11 @@
-from pathlib import Path
 from fastapi import (
     APIRouter,
-    UploadFile,
-    File,
     HTTPException,
     status
 )
-
-
 from src.utils.logs_handler import logger
 from src.utils.drift_helper import generate_drift_report
+from src.llm.llm_services import generate_drift_insights
 
 router = APIRouter(
     prefix="/drift",
@@ -29,3 +25,29 @@ def generate_report():
         "json_file": result["json_file"],
         "reference_csv_file" : result["reference_csv_file"]
     }
+    
+    
+@router.post("/insights")
+async def get_drift_insights():
+
+    try:
+
+
+        result = generate_drift_insights()
+
+        return {
+            "status": "success",
+            "message": "Drift insights generated successfully.",
+            **result
+        }
+
+    except Exception as e:
+
+        logger.exception(
+            "Failed to generate drift insights"
+        )
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to generate drift insights."
+        )    
