1f5f30fd8b43014ceb12c6c1486eb463e25c6dc5
MD ALI SHEKH <mdalishekh2003@gmail.com>
2026-08-04 11:37:01 +0530

    Writing Pytest for home endpoint

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -1,1 +1,14 @@
 # Unit Test Code for CI/CD Pipelines
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Hello World"
+    }
